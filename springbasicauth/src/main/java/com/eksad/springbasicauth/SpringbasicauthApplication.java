2c8f1d62e8a999d7f6793cc042782c3424220969
sarina <sarinahmanurung57@gmail.com>
2019-07-10 09:52:29 +0700

    first commit

---
+++ springbasicauth/src/main/java/com/eksad/springbasicauth/SpringbasicauthApplication.java
@@ -0,0 +1,13 @@
+package com.eksad.springbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicauthApplication.class, args);
+	}
+
+}
